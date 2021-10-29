483529fccc2be36a5eecbc351624d9ea6d6d0942
Seunghwanly <seunghwanly@gmail.com>
2021-10-29 17:41:51 +0900

    ➕added new components

--- client_app/lib/components/spacing.dart
+++ client_app/lib/components/spacing.dart
@@ -5,3 +5,9 @@ EdgeInsets padding({double? vertical, double? horizontal}) =>
       horizontal: horizontal ?? 0,
       vertical: vertical ?? 0,
     );
+
+EdgeInsets margin({double? vertical, double? horizontal}) =>
+    EdgeInsets.symmetric(
+      vertical: vertical ?? 0,
+      horizontal: horizontal ?? 0,
+    );
